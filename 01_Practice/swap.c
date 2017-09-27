40b8370eb07ecd8b78cea2946dafe8d2282861c8
Konstantin Mihaylov <TheMustardVader@users.noreply.github.com>
2017-09-27 23:10:03 +0300

    Update swap.c

--- 01_Practice/swap.c
+++ 01_Practice/swap.c
@@ -1,3 +1,15 @@
+//------------------------------------------------------------------------
+// NAME: Konstantin Mihaylov
+// CLASS: XIb
+// NUMBER: 13
+// PROBLEM: #3
+// FILE NAME: strdup.c
+// FILE PURPOSE: 
+// Swapping pointers.The function needs a way to refer to the original variables,
+// not copies of their values. How can we refer to other variables in C? Using pointers.
+//  The original values are not changed, because the function only swaps its own private copies.
+//------------------------------------------------------------------------
+
 #include<stdio.h>
 
 void swap(int* , int* );

3cc66b0c77d598998be8ff8b28afa21d14ec2602
Konstantin Mihaylov <TheMustardVader@users.noreply.github.com>
2017-09-27 23:02:06 +0300

    Update strdup.c

--- 01_Practice/strdup.c
+++ 01_Practice/strdup.c
@@ -1,3 +1,17 @@
+//------------------------------------------------------------------------
+// NAME: Konstantin Mihaylov
+// CLASS: XIb
+// NUMBER: 13
+// PROBLEM: #6
+// FILE NAME: strdup.c
+// FILE PURPOSE: 
+// The strdup() function, available in various languages, 
+// returns a pointer to a new string which is a duplicate of the original string.
+// http://msdn.microsoft.com/en-us/library/windows/desktop/bb759969(v=vs.85).aspx
+// https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man3/strdup.3.html
+// http://linux.die.net/man/3/strdup 
+//------------------------------------------------------------------------
+
 #include<stdio.h>
 #include <stdlib.h>
 

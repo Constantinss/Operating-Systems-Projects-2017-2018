4ee1da5b6805722ced477f6cafe6e55b29b4b531
Konstantin Mihaylov <TheMustardVader@users.noreply.github.com>
2017-09-27 23:22:04 +0300

    Update strlen.c

--- 01_Practice/strlen.c
+++ 01_Practice/strlen.c
@@ -1,3 +1,15 @@
+//------------------------------------------------------------------------
+// NAME: Konstantin Mihaylov
+// CLASS: XIb
+// NUMBER: 13
+// PROBLEM: #1
+// FILE NAME: strlen.c
+// FILE PURPOSE: 
+// The function takes a single argument,
+// the string variable whose length is to be found,
+// and returns the length of the string passed.
+//------------------------------------------------------------------------
+
 #https://elsys.github.io/os-homeworks/tasks/2017/09/25/strcat
 #https://stackoverflow.com/questions/2524611/how-can-one-print-a-size-t-variable-portably-using-the-printf-family
 
